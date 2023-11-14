08164f1012aa6d422bd041164ddc3d42a1f847be
shameerrehman <94149351+shameerrehman@users.noreply.github.com>
2023-11-14 14:58:55 -0500

    Add Cypress e2e Tests (#10)
    * test: installing cypress and setting up basic ci check
    
    * fix: cypress github action node v18 issue
    
    * fix: temporarily disable source maps
    
    ---------
    
    Co-authored-by: AleksanderBodurri <alexbodurri1117@gmail.com>

---
+++ cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts',
+  },
+  env: {
+    baseUrl: 'http://0.0.0.0:4200',
+    reactiveFormUrl: 'http://0.0.0.0:4200/reactive-form-example',
+    templateDrivenFormUrl: 'http://0.0.0.0:4200/template-driven-form-example',
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
