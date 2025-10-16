2402600adc533d3f37f8d89a88f129f572350ef4
Shiori <sut123@gmail.com>
2025-10-16 06:36:49 -0400

    Run component through prettier
    Additional prettier update

--- projects/ngx-turnstile/src/lib/types.ts
+++ projects/ngx-turnstile/src/lib/types.ts
@@ -1,12 +1,15 @@
 export {
-    TurnstileOptions,
-    Theme as TurnstileTheme,
-	AppearanceMode,
-	ExecutionMode,
-	FailureRetryMode,
-	RefreshExpiredMode,
-	RefreshTimeoutMode,
-	WidgetSize
+  TurnstileOptions,
+  Theme as TurnstileTheme,
+  AppearanceMode,
+  ExecutionMode,
+  FailureRetryMode,
+  RefreshExpiredMode,
+  RefreshTimeoutMode,
+  WidgetSize,
 } from './interfaces/turnstile-options';
 
-export { TurnstileServerValidationErrorCode, TurnstileServerValidationResponse } from './interfaces/validation-response';
+export {
+  TurnstileServerValidationErrorCode,
+  TurnstileServerValidationResponse,
+} from './interfaces/validation-response';
